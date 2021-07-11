75e11c13fb5679b0a0caffae1280fc29bf721b7d
Gaurang Ambasana <gpambasana@gmail.com>
2021-07-11 15:03:16 +0530

    added spinner component

---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,18 @@
+import React from "react";
+import spinner from "./spinner.gif";
+
+const Spinner = () => {
+  return (
+    <img
+      src={spinner}
+      alt="Loading.."
+      style={{
+        width: "200 px",
+        margin: "auto",
+        display: "block",
+      }}
+    />
+  );
+};
+
+export default Spinner;
