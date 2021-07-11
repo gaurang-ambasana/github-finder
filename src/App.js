2e4e94b3113601cbc96b48105749d62ec6564c13
Gaurang Ambasana <gpambasana@gmail.com>
2021-07-11 15:04:14 +0530

    state update using github api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React, { Component } from "react";
+import axios from "axios";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
-import UserItem from "./components/users/UserItem";
+import Users from "./components/users/Users";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  componentDidMount() {
+    this.setState({ loading: true });
+    axios
+      .get(
+        `https://api.github.com/users?client_id=${process.env.MY_GITHUB_CLIENT_ID}&client_secret=${process.env.MY_GITHUB_CLIENT_SECRET}`
+      )
+      .then((response) => {
+        this.setState({ users: response.data, loading: false });
+      });
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar />
-        <UserItem />
+        <div className="container">
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
     );
   }
