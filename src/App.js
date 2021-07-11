20acfbc6ac178807ef13acafaba41bca4aac7c74
Gaurang Ambasana <gpambasana@gmail.com>
2021-07-11 18:36:54 +0530

    added alert and routering for the app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import axios from "axios";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
+import Alert from "./components/layout/Alert";
+import About from "./components/pages/About";
 
 class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null,
   };
 
   searchUsers = (text) => {
-    this.setState({ loading: true });
+    this.setState({ loading: true, alert: null });
     axios
       .get(
         `https://api.github.com/search/users?q=${text}
@@ -24,23 +28,46 @@ class App extends Component {
       });
   };
 
-  clearUsers = () => this.setState({ users: [], loading: false });
+  clearUsers = () => this.setState({ users: [], loading: false, alert: null });
+
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+
+    setTimeout(() => {
+      this.setState({ alert: null });
+    }, 5000);
+  };
 
   render() {
-    const { users, loading } = this.state;
+    const { users, loading, alert } = this.state;
 
     return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Search
-            onSearch={this.searchUsers}
-            onClear={this.clearUsers}
-            displayClearButton={Boolean(users.length)}
-          />
-          <Users loading={loading} users={users} />
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={alert} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={(props) => (
+                  <Fragment>
+                    <Search
+                      onSearch={this.searchUsers}
+                      onClear={this.clearUsers}
+                      displayClearButton={Boolean(users.length)}
+                      onAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path="/about" component={About} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
