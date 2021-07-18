61a11bbd9a90c43c1238a9c725db051651548017
Gaurang Ambasana <gpambasana@gmail.com>
2021-07-18 17:46:40 +0530

    added reducer for github actions

---
+++ src/components/context/github/githubReducer.js
@@ -0,0 +1,42 @@
+import {
+  SEARCH_USER,
+  SET_LOADING,
+  CLEAR_USERS,
+  GET_USER,
+  GET_REPOS,
+  SET_ALERT,
+} from "../types";
+
+export default (state, action) => {
+  switch (action.type) {
+    case SEARCH_USER:
+      return {
+        ...state,
+        users: action.payload,
+        loading: false,
+      };
+
+    case CLEAR_USERS:
+      return {
+        ...state,
+        users: [],
+        loading: false,
+      };
+
+    case GET_USER:
+      return {
+        ...state,
+        user: action.payload,
+        loading: false,
+      };
+
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+
+    default:
+      return state;
+  }
+};
