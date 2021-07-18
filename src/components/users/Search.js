5d8f0a06fa6e4f73e6fc05d4dcb3247c839db00d
Gaurang Ambasana <gpambasana@gmail.com>
2021-07-18 16:18:58 +0530

    class based to function based component

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,58 +1,51 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-class Search extends Component {
-  state = {
-    text: "",
-  };
+const Search = ({ onSearch, onClear, displayClearButton, onAlert }) => {
+  const [text, setText] = useState("");
 
-  static propTypes = {
-    onSearch: PropTypes.func.isRequired,
-    onClear: PropTypes.func.isRequired,
-    displayClearButton: PropTypes.bool.isRequired,
-  };
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
 
-  render() {
-    const { onSearch, onClear, displayClearButton } = this.props;
-    const { text } = this.state;
+          if (text === "") {
+            onAlert("Search text cannot be empty", "light");
+          } else {
+            onSearch(text);
+            setText("");
+          }
+        }}
+        className="form"
+      >
+        <input
+          type="text"
+          name="text"
+          value={text}
+          placeholder="Search here...."
+          autoComplete="off"
+          onChange={(e) => setText(e.target.value)}
+        />
+        <input
+          type="submit"
+          value="Search"
+          className="btn btn-dark btn-block"
+        />
+      </form>
+      {displayClearButton && (
+        <button className="btn btn-light btn-block" onClick={onClear}>
+          Clear
+        </button>
+      )}
+    </div>
+  );
+};
 
-    return (
-      <div>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-
-            if (this.state.text === "") {
-              this.props.onAlert("Search text cannot be empty", "light");
-            } else {
-              onSearch(text);
-              this.setState({ text: "" });
-            }
-          }}
-          className="form"
-        >
-          <input
-            type="text"
-            name="text"
-            value={text}
-            placeholder="Search here...."
-            autoComplete="off"
-            onChange={(e) => this.setState({ [e.target.name]: e.target.value })}
-          />
-          <input
-            type="submit"
-            value="Search"
-            className="btn btn-dark btn-block"
-          />
-        </form>
-        {displayClearButton && (
-          <button className="btn btn-light btn-block" onClick={onClear}>
-            Clear
-          </button>
-        )}
-      </div>
-    );
-  }
-}
+Search.propTypes = {
+  onSearch: PropTypes.func.isRequired,
+  onClear: PropTypes.func.isRequired,
+  displayClearButton: PropTypes.bool.isRequired,
+};
 
 export default Search;
