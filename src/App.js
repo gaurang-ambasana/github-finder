46021933ce3660687fc1a852bb5ff3d1943945bd
Gaurang Ambasana <gpambasana@gmail.com>
2021-07-18 18:31:20 +0530

    cleaned up app.js and added cleaner routes

--- src/App.js
+++ src/App.js
@@ -1,70 +1,34 @@
-import React, { useState, Fragment } from "react";
-import axios from "axios";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
-import Users from "./components/users/Users";
-import Search from "./components/users/Search";
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
 import User from "./components/users/User";
 import GithubState from "./components/context/github/GithubState";
+import AlertState from "./components/context/alert/AlertState";
 
 const App = () => {
-  const [alert, setAlert] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [repos, setRepos] = useState([]);
-
-  const getUserRepos = (username) => {
-    setLoading(true);
-    setAlert(null);
-
-    axios
-      .get(
-        `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&
-         client_id=${process.env.MY_GITHUB_CLIENT_ID}&client_secret=${process.env.MY_GITHUB_CLIENT_SECRET}`
-      )
-      .then((response) => {
-        setRepos(response.data);
-        setLoading(false);
-      });
-  };
-
-  const showAlert = (msg, type) => {
-    setAlert({ msg, type });
-    setTimeout(() => setAlert(null), 5000);
-  };
-
   return (
     <GithubState>
-      <Router>
-        <div className="App">
-          <Navbar />
-          <div className="container">
-            <Alert alert={alert} />
-            <Switch>
-              <Route
-                exact
-                path="/"
-                render={(props) => (
-                  <Fragment>
-                    <Search onAlert={showAlert} />
-                    <Users />
-                  </Fragment>
-                )}
-              />
-              <Route exact path="/about" component={About} />
-              <Route
-                exact
-                path="/users/:login"
-                render={(props) => (
-                  <User {...props} getRepos={getUserRepos} repos={repos} />
-                )}
-              />
-            </Switch>
+      <AlertState>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Alert />
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/users/:login" component={User} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </AlertState>
     </GithubState>
   );
 };
